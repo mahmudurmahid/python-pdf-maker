9495678bb3804d0c42a06a002c8a42d2bd0ff83c
mahmudurmahid <m.mahid@icloud.com>
2025-05-08 16:06:37 +0100

    debug blank pages with no lines

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.set_auto_page_break(auto=False, margin=0)
-
+# create dataframe from csv
 df = pd.read_csv("topics.csv")
-
 
 for index, row in df.iterrows():
     pdf.add_page()
@@ -20,19 +19,19 @@ for index, row in df.iterrows():
     for y in range(20, 298, 10):
         pdf.line(x1=10, y1=y, x2=200, y2=y)
 
-    # footer on each new topic
-    pdf.ln(265)
-    pdf.set_font(family="Times", style="I", size=10)
-    pdf.set_text_color(180, 180, 180)
-    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+        # footer on each new topic
+        pdf.ln(265)
+        pdf.set_font(family="Times", style="I", size=10)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
     # pages for each topic based on Pages row in topics.csv
     for i in range(row["Pages"] - 1):
         pdf.add_page()
     
-    # lines on the page
-    for y in range(20, 298, 10):
-        pdf.line(x1=10, y1=y, x2=200, y2=y)
+        # lines on the page
+        for y in range(20, 298, 10):
+            pdf.line(x1=10, y1=y, x2=200, y2=y)
 
         # footer on every page except new topic page
         pdf.ln(275)
